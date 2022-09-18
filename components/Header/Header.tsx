d8516fcbd9c49ab7d7585074900423f0dbed259b
Michael Bruno <brunomike254@gmail.com>
2022-09-18 23:41:39 +0300

    clean up

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -13,13 +13,12 @@ export default function Header() {
         setIsOpen(!isOpen)
     }
     return (
-        <header className={styles.header}>
-            <Link href="/">
-                {/* <Image src={Logo} alt="Musicbox" className={styles.logo} width={100} height={45} /> */}
+        <header className='flex justify-around items-center bg-black h-16 absolute top-0 left-0 w-screen z-12 text-white font-bold'>
+            <Link href="/">                
                 <Image src={Logo} alt="Musicbox" className="hover:cursor-pointer" width={100} height={45} />
             </Link>
             <nav className={styles.navLinks}>
-                <ul>
+                <ul className='flex items-center gap-16'>
                     <li>
                         <Link href={"#"}><a href="#">Download</a></Link>
                         
@@ -29,36 +28,36 @@ export default function Header() {
                     </li>
                     <li >
                         <Link href="/signin">
-                        <a href="#" className={`${styles.btn} ${styles.login}`}>Log in</a>
+                        <a href="#" className='inline-block px-16 py-2 border-2 border-white rounded-full'>Log in</a>
                         </Link>
                     </li>
                     <li >
                     <Link href="/signup">
-                        <a href="" className={`${styles.btn} ${styles.signup}`}>Sign up</a>
+                        <a href="" className='inline-block px-16 py-2 bg-white text-black rounded-full'>Sign up</a>
                         </Link>
                     </li>
                 </ul>
             </nav>
             <div className="app__navbar-mobile">
-                <button className={styles.toggleClosed} onClick={toggleNavbar}>
+                <button className='w-30 h-30 rounded-full border-none outline-none' onClick={toggleNavbar}>
                     <HiMenuAlt4/>
                 </button>
                 {isOpen&&(
-                    <nav className={styles.navLinksMobile}>
-                        <button className={styles.toggleOpen} onClick={toggleNavbar}><HiX/></button>
-                        <ul>
+                    <nav className='absolute bg-white gap-8 right-0 top-0 h-screen w-10/12 text-black z-5'>
+                        <button className='outline-none border-none w-35 h-35 rounded-full bg-none' onClick={toggleNavbar}><HiX/></button>
+                        <ul className='flex-col items-center justify-center gap-16'>
                             <li>
-                                <Link href="#"><a href="#">Download</a></Link>
+                                <Link href="#" >Download</Link>
 
                             </li>
                             <li>
-                                <a href="#">Help</a>
+                            <Link href="#">Help</Link>
                             </li>
                             <li >
-                                <a href="#" className={`${styles.btn} ${styles.login}`}>Log in</a>
+                                <a href="#" className='inline-block px-16 py-2 border-2 border-white'>Log in</a>
                             </li>
                             <li >
-                                <a href="#" className={`${styles.btn} ${styles.signup}`}>Sign up</a>
+                                <a href="#" className='inline-block px-16 py-2 border-2'>Sign up</a>
                             </li>
                         </ul>
                     </nav>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,20 @@ module.exports = {
     extend: {
       colors: {
         'bluish': '#2DCEEF',
-        'purple_1':'#9B2DEF',
-        'bluish_1':'#35EDFB'
+        'purple_1': '#9B2DEF',
+        'bluish_1': '#35EDFB'
       },
+      backgroundImage: theme => ({
+        'phone_finder': "url('/assets/images/phone_finder.png')",        
+      }),
+      screens:{
+        'xs':'280px',
+        'sm':'640px',
+        'md':'768px',
+        'lg':'1024px',
+        'xl':'1280px',
+        '2xl':'1536px',
+      }
     },
   },
   plugins: [],
