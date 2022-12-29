d82819439b49c232deffab791d9b018d21d864b6
Michael Bruno <brunomike254@gmail.com>
2022-12-29 10:27:29 +0300

    resolved secrets incident

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
+import secrets from './secrets.json';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  env: {
-    apiUrl: "http://localhost:2000",
-    GOOGLE_RECAPTCHA_SITE_KEY:"6LeYocAZAAAAAJHssMd0OslkAv0EswCKIP8jhkA-",
-    GOOGLE_RECAPTCHA_SECRET:"6LeYocAZAAAAAOgtkOfeG0kcebVfXpOFEIOV76My",
-  }
+  env: JSON.parse(secrets)
 }
 
 module.exports = {
