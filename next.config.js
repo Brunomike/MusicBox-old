c4fabd992ccab1e8ead71696edffdac241856af2
Michael Bruno <brunomike254@gmail.com>
2022-12-29 13:00:04 +0300

    fixed bug

--- next.config.js
+++ next.config.js
@@ -8,7 +8,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
-    apiUrl:"http://localhost:2000"
+    apiUrl:"http://localhost:2000",
+    GOOGLE_RECAPTCHA_SITE_KEY:"",
+    GOOGLE_RECAPTCHA_SECRET:""
   }
 }
 
