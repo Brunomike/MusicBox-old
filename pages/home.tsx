97c2cb0fabc29d723b25de61ff6451dc1bf4c3f2
Michael Bruno <brunomike254@gmail.com>
2023-01-12 12:17:50 +0300

    fixed import bug

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from 'next';
-import { Head } from 'next/document';
+import Head from 'next/head';
 import Header from '../components/Header/Header';
 
 const Home: NextPage = () => {
